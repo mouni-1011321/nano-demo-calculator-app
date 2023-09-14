4a14b109571e85765fdbde815972047c268500eb
mouni-1011321 <pmounika9502@gmail.com>
2023-09-15 05:03:09 +0530

    add and substract numbers

--- server.js
+++ server.js
@@ -10,28 +10,18 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send("Hello, World!");
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
     const { first, second } = req.body;
-    if(typeof first !== 'number' || typeof second !== 'number') {
-        res.status(400).send("Invalid input. Both first and second must be numbers.");
-    } else {
-        const result = first + second;
-        res.status(200).json({ result });
-    }
+    res.json({ "result": first + second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
     const { first, second } = req.body;
-    if(typeof first !== 'number' || typeof second !== 'number') {
-        res.status(400).send("Invalid input. Both first and second must be numbers.");
-    } else {
-        const result = first - second;
-        res.status(200).json({ result });
-    }
+    res.json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
